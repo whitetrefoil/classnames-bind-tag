c9163696565e29528e00520a14f6bb147c129bd3
WhiteTrefoil <whitetrefoil@gmail.com>
2018-12-05 10:48:21 +0800

    ignore built code from source

--- lib/types/main.d.ts
+++
@@ -1,6 +0,0 @@
-import { ClassNamesExport } from 'classnames/bind';
-export interface BoundCss {
-    cx: ClassNamesExport;
-    c(strings: TemplateStringsArray): string;
-}
-export default function bindCss(styles: Record<string, string>): BoundCss;
