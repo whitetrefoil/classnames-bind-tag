12aeccab2a92be815cbb8e59df511e40b48eee65
WhiteTrefoil <whitetrefoil@gmail.com>
2018-12-05 14:00:59 +0800

    better compatibility

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,30 @@ import classNames, { ClassNamesExport } from 'classnames/bind'
 
 export interface BoundCss {
   cx: ClassNamesExport
-  c(strings: TemplateStringsArray): string
+  c(strings: TemplateStringsArray, ...exps: string[]): string
 }
 
 export default function bindCss(styles: Record<string, string>): BoundCss {
   const cx = classNames.bind(styles)
 
-  function c(strings: TemplateStringsArray) {
-    if (strings != null && strings.raw != null && strings.raw.length > 0) {
-      return cx(strings.raw[0].split(' '))
+  function c(strings: TemplateStringsArray, ...exps: string[]): string {
+    if (strings == null || strings.raw == null) {
+      return ''
     }
 
-    return ''
+    let split: string[] = []
+    for (const str of strings.raw) {
+      const processed = str.trim().split(' ')
+      if (processed.length > 0) {
+        split = split.concat(processed)
+      }
+    }
+
+    const strPart = cx(split)
+    const regexpPart = exps.join(' ').trim()
+    const splitter   = strPart.length > 0 && regexpPart.length > 0 ? ' ' : ''
+
+    return strPart + splitter + regexpPart
   }
 
   return { cx, c }
